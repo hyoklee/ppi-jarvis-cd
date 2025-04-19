3ff73e957b637ed8818844591bf1c7df1be66ad4
H. Joe Lee <hyoklee@hdfgroup.org>
2025-04-19 01:29:35 -0500

    fix: missing module error

--- jarvis_cd/basic/pkg.py
+++ jarvis_cd/basic/pkg.py
@@ -73,8 +73,9 @@ class PipelineIterator:
         self.repeat = ppl.config['iterator']['repeat']
         ppl.set_config_env_vars()
         self.iter_out = os.path.expandvars(ppl.config['iterator']['output'])
-        print(f'ITER OUT: {self.iter_out} '
-              f'(from: {ppl.config['iterator']['output']})')
+        print(f'ITER OUT: {self.iter_out} ')
+        o = ppl.config['iterator']['output']
+        print(f'from: {o}')
         self.stats_path = f'{self.iter_out}/stats_dict.csv'
         self.stats = []
 
